e1237fb7c48845772832e83130332cdd928a5773
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-05 17:44:15 +0200

    invite users to account

--- app/controllers/account_users_controller.rb
+++ app/controllers/account_users_controller.rb
@@ -6,6 +6,20 @@ class AccountUsersController < ApplicationController
     @account_users = @current_account.account_users
   end
 
+  def new
+  end
+
+  def create
+    email = params[:email]
+    return redirect_to account_account_users_path(@current_account), alert: 'No email provided' if email.blank?
+
+    user = User.find_by(email:) || User.invite!({ email: }, current_user)
+    return redirect_to account_account_users_path(@current_account), alert: 'Email invalid' unless user.valid?
+
+    user.account_users.find_or_create_by(account: @current_account, role: AccountUser.roles[:member])
+    redirect_to account_account_users_path(@current_account), notice: "#{email} invited!"
+  end
+
   private
 
   def set_current_account
