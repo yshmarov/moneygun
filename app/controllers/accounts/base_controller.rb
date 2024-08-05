d58e5b99bcfbfe2ee20504a4bec2da80824e6b89
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-05 23:02:45 +0200

    display current_account.name in navbar

--- app/controllers/accounts/base_controller.rb
+++ app/controllers/accounts/base_controller.rb
@@ -1,7 +1,9 @@
 class Accounts::BaseController < ApplicationController
+  # the order of the before_actions is important
   before_action :authenticate_user!
   before_action :set_account
   before_action :authorize_account_user!
+  before_action :set_current_account_user
 
   def set_account
     @account = current_user.accounts.find(params[:account_id])
@@ -11,11 +13,11 @@ class Accounts::BaseController < ApplicationController
     redirect_to root_path, alert: "You are not authorized to perform this action." unless @account.users.include?(current_user)
   end
 
-  def current_account_user
+  def set_current_account_user
     @current_account_user ||= current_user.account_users.find_by(account: @account)
   end
 
   def authorize_account_admin!
-    redirect_to root_path, alert: "You are not authorized to perform this action." unless current_account_user.admin?
+    redirect_to root_path, alert: "You are not authorized to perform this action." unless @current_account_user.admin?
   end
 end
