d3a311783c7a20bca3c0801da3aa2d882517b81e
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-05 18:21:24 +0200

    account_user - validate unique account-user combination

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -3,4 +3,9 @@ class AccountUser < ApplicationRecord
   belongs_to :user
 
   enum :role, { member: "member", admin: "admin" }
+
+  validates_uniqueness_of :user_id, scope: :account_id
+  validates_uniqueness_of :account_id, scope: :user_id
+
+  validates :role, presence: true
 end
--- test/models/account_user_test.rb
+++ test/models/account_user_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class AccountUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "unique user-account combination" do
+    account_user = AccountUser.new(account: accounts(:one), user: users(:one))
+    assert_not account_user.valid?
+    assert_includes account_user.errors.messages[:user_id], "has already been taken"
+    assert_includes account_user.errors.messages[:account_id], "has already been taken"
+  end
 end
