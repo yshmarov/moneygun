30e50f9f67d5524766cf8a99f49957459c717500
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-10 10:42:07 +0200

    UI: add active_link_to; footer always on bottom

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_link_to(text = nil, path = nil, active_classes: '', **options, &)
+    path ||= text
+
+    classes = active_classes.presence || 'font-bold underline'
+    options[:class] = class_names(options[:class], classes) if current_page?(path)
+
+    return link_to(path, options, &) if block_given?
+
+    link_to text, path, options
+  end
 end
