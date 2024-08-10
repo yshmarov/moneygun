8d365003c00ecee7263ae3126d28e7c22cc3120b
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-10 11:17:09 +0200

    UI: add basic flash msg styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,19 @@
 module ApplicationHelper
-  def active_link_to(text = nil, path = nil, active_classes: '', **options, &)
+  def active_link_to(text = nil, path = nil, active_classes: "", **options, &)
     path ||= text
 
-    classes = active_classes.presence || 'font-bold underline'
+    classes = active_classes.presence || "font-bold underline"
     options[:class] = class_names(options[:class], classes) if current_page?(path)
 
     return link_to(path, options, &) if block_given?
 
     link_to text, path, options
+  end
+
+  def flash_style(type)
+    case type
+    when "notice" then "bg-green-100 border-green-400 text-green-700"
+    when "alert" then "bg-red-100 border-red-400 text-red-700"
+    end
   end
 end
---
+++ app/javascript/controllers/element_remove_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+// <div data-controller="element-remove" data-action="click->element-remove#click">Click me to remove me</div>
+// Connects to data-controller="element-remove"
+export default class extends Controller {
+  click() {
+    this.element.remove()
+  }
+}
