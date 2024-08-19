e690f7c8fb453b4c51de9c6c32641b10b6dcaf7a
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-19 11:36:13 +0200

    accounts - basic styling

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,12 +1,16 @@
 module AccountsHelper
   def account_avatar_link(account)
     active_link_to account_path(account), class: "inline-flex items-center gap-2" do
-      if account.logo.attached?
-        image_tag account.logo, class: "rounded-full size-8"
-      else
-        tag.div account.name[0], class: "size-8 uppercase rounded-full bg-gray-300 text-white flex items-center justify-center"
-      end +
+      account_avatar(account) +
       account.name
+    end
+  end
+
+  def account_avatar(account)
+    if account.logo.attached?
+      image_tag account.logo, class: "rounded-full size-8"
+    else
+      tag.div account.name[0], class: "size-8 uppercase rounded-full bg-gray-300 text-white flex items-center justify-center"
     end
   end
 end
