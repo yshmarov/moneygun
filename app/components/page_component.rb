3af4e9d828b02040db8f8a3cca96bc27d2870981
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-22 10:35:51 +0200

    PageComponent content_container

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -3,9 +3,14 @@
 class PageComponent < ViewComponent::Base
   renders_one :action_list
 
-  def initialize(title:)
+  def initialize(title:, content_container: false)
     @title = title
+    @content_container = content_container
   end
 
   attr_reader :title
+
+  def conent_container_class
+    "border rounded-lg p-4" if @content_container == true
+  end
 end
