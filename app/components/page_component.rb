e08bd5bbe2523bded714fa530041199aa864cab3
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-22 10:43:05 +0200

    PageComponent full_width
    not the final variation of widths

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -3,14 +3,23 @@
 class PageComponent < ViewComponent::Base
   renders_one :action_list
 
-  def initialize(title:, content_container: false)
+  def initialize(title:, content_container: false, full_width: true)
     @title = title
     @content_container = content_container
+    @full_width = full_width
   end
 
   attr_reader :title
 
   def conent_container_class
     "border rounded-lg p-4" if @content_container == true
+  end
+
+  def width_class
+    if @full_width == true
+      "md:w-2/3 w-full"
+    else
+      "max-w-md w-full"
+    end
   end
 end
