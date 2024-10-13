3de51272bd7a4a928c8a609a8a75130a10b8ee25
Yaro Shm <yshmarov@gmail.com>
2024-10-13 22:52:18 +0200

    account logo - 2 letters, not 1

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -3,7 +3,7 @@ module AccountsHelper
     if account.logo.attached?
       image_tag account.logo, class: "rounded-full size-8"
     else
-      tag.div account.name[0], class: "size-8 uppercase rounded-full bg-gray-300 text-white flex items-center justify-center"
+      tag.div account.name[0..1], class: "size-8 uppercase rounded-full bg-gray-300 text-white text-sm flex items-center justify-center"
     end
   end
 end
