b6563f2bfac68a4f0094d1424a7b157f61fec50d
Yaro Shm <yshmarov@gmail.com>
2024-10-14 10:25:17 +0200

    account_avatar - add image alt

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,7 +1,7 @@
 module AccountsHelper
   def account_avatar(account)
     if account.logo.attached?
-      image_tag account.logo, class: "rounded-full size-8"
+      image_tag account.logo, class: "rounded-full size-8", alt: account.name
     else
       tag.div account.name[0..1], class: "size-8 uppercase rounded-full bg-gray-300 text-white text-sm flex items-center justify-center"
     end
