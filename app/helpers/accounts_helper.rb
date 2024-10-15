1def9af64a82da412d2bf11f4b60be754ce5ddad
Yaro Shm <yshmarov@gmail.com>
2024-10-15 02:10:06 +0200

    sidebar styling - more like Slack

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,9 +1,9 @@
 module AccountsHelper
   def account_avatar(account)
     if account.logo.attached?
-      image_tag account.logo, class: "rounded-full size-8", alt: account.name
+      image_tag account.logo, class: "rounded size-8", alt: account.name
     else
-      tag.div account.name[0..1], class: "size-8 uppercase rounded-full bg-gray-300 text-white text-sm flex items-center justify-center"
+      tag.div account.name[0..1], class: "size-8 uppercase rounded bg-gray-600 text-white text-xs flex items-center justify-center"
     end
   end
 end
