1a76301a7b448fe7800e769b8e3c9979050df045
Yaro Shm <yshmarov@gmail.com>
2024-10-17 08:44:39 +0200

    scaffold generator try 1

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Askvote
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # config.generators.template_engine = :erb
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
