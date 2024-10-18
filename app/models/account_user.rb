9c8e18fa852dc8fe4c3893e6cc70d039467e1291
Yaro Shm <yshmarov@gmail.com>
2024-10-19 00:57:41 +0200

    Update account_user.rb

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -8,11 +8,22 @@ class AccountUser < ApplicationRecord
   validates_uniqueness_of :account_id, scope: :user_id
 
   validates :role, presence: true
+  validate :cannot_change_role_if_only_admin, on: :update
 
   def try_destroy
     return false if account.account_users.count == 1
     return false if role == "admin" && account.account_users.where(role: "admin").count == 1
 
     destroy
+  end
+
+  private
+
+  def cannot_change_role_if_only_admin
+    return if account.account_users.where(role: "admin").count > 1
+
+    if role_changed? && role_was == "admin"
+      errors.add(:base, "Role cannot be changed because this is the only admin.")
+    end
   end
 end
