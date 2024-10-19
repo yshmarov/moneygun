ffab700daef8a7ffc9cc06fba499b2ea0fc186eb
Yaro Shm <yshmarov@gmail.com>
2024-10-19 13:17:30 +0200

    test current user deletion flow
    this is not an ideal state!

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # TODO: account_users with associated downstream records should not be deletable
+  test "destroying a user destroys associated account_users" do
+    account = accounts(:one)
+    user = users(:one)
+
+    assert_no_difference "Account.count" do
+      assert_difference "AccountUser.count", -1 do
+        user.destroy
+      end
+    end
+
+    # TODO: this should not be a valid state!
+    assert account.account_users.none?
+  end
 end
