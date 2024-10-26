10e562fbb6a41fd1c727a70e16171c4380c364f3
Yaro Shm <yshmarov@gmail.com>
2024-10-26 19:55:07 +0200

    rename app to Moneygun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "askvote_production"
+  # config.active_job.queue_name_prefix = "moneygun_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
