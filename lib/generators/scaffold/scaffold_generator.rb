96db74a1e0e28b121ff862f5b3b02b4c41b0d288
Yaro Shm <yshmarov@gmail.com>
2024-10-27 08:45:26 +0100

    sidebar - abstract nav links

--- lib/generators/scaffold/scaffold_generator.rb
+++ lib/generators/scaffold/scaffold_generator.rb
@@ -4,27 +4,14 @@ class ScaffoldGenerator < Rails::Generators::NamedBase
   hook_for :scaffold, in: :rails, default: true, type: :boolean
 
   def add_to_navigation
-    inject_into_file "app/views/shared/_sidebar.html.erb", before: "<%# add new resources here %>" do
+    append_to_file "app/views/shared/_nav_links.html.erb" do
       <<-ERB
-      <%= active_link_to account_#{index_helper(type: :path)}(@account), class_active: "bg-gray-300", class: "w-full btn btn-transparent" do %>
-        <%= inline_svg_tag "svg/question-mark-circle.svg", class: "size-5" %>
-        <span>
-        <%= plural_table_name.titleize %>
-        </span>
-      <% end %>
-      ERB
-    end
-  end
-
-  def add_to_sidebar_2_cols
-    inject_into_file "app/views/shared/_sidebar_2_cols.html.erb", before: "<%# add new resources here %>" do
-      <<-ERB
-      <%= active_link_to account_#{index_helper(type: :path)}(@account), class_active: "bg-gray-300", class: "btn btn-transparent" do %>
-        <%= inline_svg_tag "svg/question-mark-circle.svg", class: "size-5" %>
-        <span>
-        <%= plural_table_name.titleize %>
-        </span>
-      <% end %>
+<%= active_link_to #{index_helper(type: :path)}, class_active: "bg-gray-300", class: "w-full btn btn-transparent" do %>
+  <%= inline_svg_tag "svg/question-mark-circle.svg", class: "size-5" %>
+  <span>
+  #{plural_table_name.titleize}
+  </span>
+<% end %>
       ERB
     end
   end
