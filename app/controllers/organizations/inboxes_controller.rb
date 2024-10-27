dd69d52e20d54e9aa2ea75d4e03af5f0cb515ac3
Yaro Shm <yshmarov@gmail.com>
2024-10-27 14:57:24 +0100

    clean up unused code

--- app/controllers/organizations/inboxes_controller.rb
+++ app/controllers/organizations/inboxes_controller.rb
@@ -1,73 +1,60 @@
 class Organizations::InboxesController < Organizations::BaseController
   before_action :set_inbox, only: %i[ show edit update destroy ]
 
-  # GET /inboxes or /inboxes.json
+  # GET /organizations/1/inboxes
   def index
     @inboxes = @organization.inboxes
     authorize @organization, policy_class: InboxPolicy
   end
 
-  # GET /inboxes/1 or /inboxes/1.json
+  # GET /organizations/1/inboxes/1
   def show
   end
 
-  # GET /inboxes/new
+  # GET /organizations/1/inboxes/new
   def new
     @inbox = @organization.inboxes.new
     authorize @inbox
   end
 
-  # GET /inboxes/1/edit
+  # GET /organizations/1/inboxes/1/edit
   def edit
   end
 
-  # POST /inboxes or /inboxes.json
+  # POST /organizations/1/inboxes
   def create
     @inbox = @organization.inboxes.new(inbox_params)
     authorize @inbox
 
-    respond_to do |format|
-      if @inbox.save
-        format.html { redirect_to organization_inbox_url(@organization, @inbox), notice: "Inbox was successfully created." }
-        format.json { render :show, status: :created, location: @inbox }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @inbox.errors, status: :unprocessable_entity }
-      end
+    if @inbox.save
+      redirect_to organization_inbox_url(@organization, @inbox), notice: "Inbox was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /inboxes/1 or /inboxes/1.json
+  # PATCH/PUT /organizations/1/inboxes/1
   def update
-    respond_to do |format|
-      if @inbox.update(inbox_params)
-        format.html { redirect_to organization_inbox_url(@organization, @inbox), notice: "Inbox was successfully updated." }
-        format.json { render :show, status: :ok, location: @inbox }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @inbox.errors, status: :unprocessable_entity }
-      end
+    if @inbox.update(inbox_params)
+      redirect_to organization_inbox_url(@organization, @inbox), notice: "Inbox was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /inboxes/1 or /inboxes/1.json
+  # DELETE /organizations/1/inboxes/1
   def destroy
     @inbox.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to organization_inboxes_url(@organization), notice: "Inbox was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to organization_inboxes_url(@organization), notice: "Inbox was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_inbox
       @inbox = @organization.inboxes.find(params[:id])
       authorize @inbox
     end
 
-    # Only allow a list of trusted parameters through.
     def inbox_params
       params.require(:inbox).permit(:name)
     end
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,12 +1,12 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: %i[show edit update destroy]
 
-  # GET /organizations or /organizations.json
+  # GET /organizations
   def index
     @organizations = current_user.organizations.includes(:users)
   end
 
-  # GET /organizations/1 or /organizations/1.json
+  # GET /organizations/1
   def show
   end
 
@@ -19,43 +19,32 @@ class OrganizationsController < ApplicationController
   def edit
   end
 
-  # POST /organizations or /organizations.json
+  # POST /organizations
   def create
     @organization = Organization.new(organization_params)
     @organization.memberships.build(user: current_user, role: Membership.roles[:admin])
 
-    respond_to do |format|
-      if @organization.save
-        format.html { redirect_to organization_url(@organization), notice: "Organization was successfully created." }
-        format.json { render :show, status: :created, location: @organization }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
+    if @organization.save
+      redirect_to organization_url(@organization), notice: "Organization was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /organizations/1 or /organizations/1.json
+  # PATCH/PUT /organizations/1
   def update
-    respond_to do |format|
-      if @organization.update(organization_params)
-        format.html { redirect_to organization_url(@organization), notice: "Organization was successfully updated." }
-        format.json { render :show, status: :ok, location: @organization }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
+    if @organization.update(organization_params)
+      redirect_to organization_url(@organization), notice: "Organization was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /organizations/1 or /organizations/1.json
+  # DELETE /organizations/1
   def destroy
     @organization.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to organizations_url, notice: "Organization was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to organizations_url, notice: "Organization was successfully destroyed."
   end
 
   private
