509ee932bcf6c768b24fc59757d9f6b5086b13bb
Yaro Shm <yshmarov@gmail.com>
2024-10-27 12:26:39 +0100

    lint

--- config/initializers/acts_as_tenant.rb
+++ config/initializers/acts_as_tenant.rb
@@ -2,5 +2,5 @@ ActsAsTenant.configure do |config|
   config.require_tenant = true
 
   # Customize the query for loading the tenant in background jobs
-  config.job_scope = ->{ all }
+  config.job_scope = -> { all }
 end
