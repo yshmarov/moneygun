6de994cfc1ed23e9f402cb1e7f368acbf9ded159
Yaro Shm <yshmarov@gmail.com>
2024-12-10 08:12:14 +0100

    Update transfer_test.rb

--- test/models/organization/transfer_test.rb
+++ test/models/organization/transfer_test.rb
@@ -1,4 +1,16 @@
 require "test_helper"
 
 class OrganizationTransferTest < ActiveSupport::TestCase
+  test "transfer_ownership" do
+    organization = organizations(:one)
+    user = users(:two)
+
+    assert_not organization.transfer_ownership(user.id)
+    assert_not organization.owner?(user)
+
+    membership = organization.memberships.create!(user: user, role: Membership.roles[:member])
+    assert organization.transfer_ownership(user.id)
+    assert organization.owner?(user)
+    assert membership.reload.admin?
+  end
 end
