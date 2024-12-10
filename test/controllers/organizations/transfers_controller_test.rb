75badefeda10d464bc492f7f86e966bb3e72d0f7
Yaro Shm <yshmarov@gmail.com>
2024-12-10 08:24:44 +0100

    Update transfers_controller_test.rb

--- test/controllers/organizations/transfers_controller_test.rb
+++ test/controllers/organizations/transfers_controller_test.rb
@@ -1,4 +1,52 @@
 require "test_helper"
 
 class Organizations::TransfersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    @organization = organizations(:one)
+    @membership = memberships(:one)
+
+    @user2 = users(:two)
+  end
+
+  test "#show" do
+    sign_in @user
+
+    get organization_transfer_path(@organization)
+    assert_response :success
+  end
+
+  test "admin can transfer organization" do
+    @membership2 = @organization.memberships.create!(user: @user2, role: Membership.roles[:member])
+    sign_in @user
+
+    patch organization_transfer_path(@organization), params: { user_id: @user2.id }
+    assert_redirected_to organization_path(@organization)
+    assert_equal @user2, @organization.reload.owner
+  end
+
+  test "regular_user cannot transfer organization" do
+    @membership2 = @organization.memberships.create!(user: @user2, role: Membership.roles[:member])
+    sign_in @user2
+
+    patch organization_transfer_path(@organization), params: { user_id: @user2.id }
+    assert_redirected_to organization_path(@organization)
+    assert_equal @user, @organization.reload.owner
+  end
+
+  test "transfer organization to non-member" do
+    sign_in @user
+
+    patch organization_transfer_path(@organization), params: { user_id: @user2.id }
+    assert_response :unprocessable_entity
+    assert_equal @user, @organization.reload.owner
+  end
+
+  test "transfer organization that user is not a member of" do
+    sign_in @user2
+
+    patch organization_transfer_path(@organization), params: { user_id: @user2.id }
+    assert_response :not_found
+    assert_equal @user, @organization.reload.owner
+  end
 end
