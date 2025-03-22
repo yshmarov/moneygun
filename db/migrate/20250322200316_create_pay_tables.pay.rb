52e9b3f1434c9202808509c4f90508fbb7df9c04
Yaro Shm <yshmarov@gmail.com>
2025-03-23 00:21:18 +0100

    lint

--- db/migrate/20250322200316_create_pay_tables.pay.rb
+++ db/migrate/20250322200316_create_pay_tables.pay.rb
@@ -13,8 +13,8 @@ class CreatePayTables < ActiveRecord::Migration[6.0]
       t.datetime :deleted_at
       t.timestamps
     end
-    add_index :pay_customers, [:owner_type, :owner_id, :deleted_at], name: :pay_customer_owner_index, unique: true
-    add_index :pay_customers, [:processor, :processor_id], unique: true
+    add_index :pay_customers, [ :owner_type, :owner_id, :deleted_at ], name: :pay_customer_owner_index, unique: true
+    add_index :pay_customers, [ :processor, :processor_id ], unique: true
 
     create_table :pay_merchants, id: primary_key_type do |t|
       t.belongs_to :owner, polymorphic: true, index: false, type: foreign_key_type
@@ -24,10 +24,10 @@ class CreatePayTables < ActiveRecord::Migration[6.0]
       t.public_send Pay::Adapter.json_column_type, :data
       t.timestamps
     end
-    add_index :pay_merchants, [:owner_type, :owner_id, :processor]
+    add_index :pay_merchants, [ :owner_type, :owner_id, :processor ]
 
     create_table :pay_payment_methods, id: primary_key_type do |t|
-      t.belongs_to :customer, foreign_key: {to_table: :pay_customers}, null: false, index: false, type: foreign_key_type
+      t.belongs_to :customer, foreign_key: { to_table: :pay_customers }, null: false, index: false, type: foreign_key_type
       t.string :processor_id, null: false
       t.boolean :default
       t.string :type
@@ -35,10 +35,10 @@ class CreatePayTables < ActiveRecord::Migration[6.0]
       t.string :stripe_account
       t.timestamps
     end
-    add_index :pay_payment_methods, [:customer_id, :processor_id], unique: true
+    add_index :pay_payment_methods, [ :customer_id, :processor_id ], unique: true
 
     create_table :pay_subscriptions, id: primary_key_type do |t|
-      t.belongs_to :customer, foreign_key: {to_table: :pay_customers}, null: false, index: false, type: foreign_key_type
+      t.belongs_to :customer, foreign_key: { to_table: :pay_customers }, null: false, index: false, type: foreign_key_type
       t.string :name, null: false
       t.string :processor_id, null: false
       t.string :processor_plan, null: false
@@ -59,13 +59,13 @@ class CreatePayTables < ActiveRecord::Migration[6.0]
       t.string :payment_method_id
       t.timestamps
     end
-    add_index :pay_subscriptions, [:customer_id, :processor_id], unique: true
-    add_index :pay_subscriptions, [:metered]
-    add_index :pay_subscriptions, [:pause_starts_at]
+    add_index :pay_subscriptions, [ :customer_id, :processor_id ], unique: true
+    add_index :pay_subscriptions, [ :metered ]
+    add_index :pay_subscriptions, [ :pause_starts_at ]
 
     create_table :pay_charges, id: primary_key_type do |t|
-      t.belongs_to :customer, foreign_key: {to_table: :pay_customers}, null: false, index: false, type: foreign_key_type
-      t.belongs_to :subscription, foreign_key: {to_table: :pay_subscriptions}, null: true, type: foreign_key_type
+      t.belongs_to :customer, foreign_key: { to_table: :pay_customers }, null: false, index: false, type: foreign_key_type
+      t.belongs_to :subscription, foreign_key: { to_table: :pay_subscriptions }, null: true, type: foreign_key_type
       t.string :processor_id, null: false
       t.integer :amount, null: false
       t.string :currency
@@ -76,7 +76,7 @@ class CreatePayTables < ActiveRecord::Migration[6.0]
       t.string :stripe_account
       t.timestamps
     end
-    add_index :pay_charges, [:customer_id, :processor_id], unique: true
+    add_index :pay_charges, [ :customer_id, :processor_id ], unique: true
 
     create_table :pay_webhooks, id: primary_key_type do |t|
       t.string :processor
@@ -93,6 +93,6 @@ class CreatePayTables < ActiveRecord::Migration[6.0]
     setting = config.options[config.orm][:primary_key_type]
     primary_key_type = setting || :primary_key
     foreign_key_type = setting || :bigint
-    [primary_key_type, foreign_key_type]
+    [ primary_key_type, foreign_key_type ]
   end
 end
