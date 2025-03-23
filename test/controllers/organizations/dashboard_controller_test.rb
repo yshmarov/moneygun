f9be94a2011f612c02e963e06218de3208ac5c62
Yaro Shm <yshmarov@gmail.com>
2025-03-23 20:20:23 +0100

    Update dashboard_controller_test.rb

--- test/controllers/organizations/dashboard_controller_test.rb
+++ test/controllers/organizations/dashboard_controller_test.rb
@@ -6,4 +6,17 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     get organization_dashboard_url(organizations(:one))
     assert_response :success
   end
+
+  test "only subscribed users can access the paywalled page" do
+    user = users(:one)
+    organization = organizations(:one)
+    sign_in user
+    get organization_paywalled_page_url(organization)
+    assert_redirected_to organization_subscriptions_url(organization)
+
+    organization.set_payment_processor :fake_processor, allow_fake: true
+    organization.payment_processor.subscribe(plan: "fake")
+    get organization_paywalled_page_url(organization)
+    assert_response :success
+  end
 end
