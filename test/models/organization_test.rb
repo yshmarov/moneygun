fa1ada9756482ac55a20d98b17002af09e7ca567
Yaro Shm <yshmarov@gmail.com>
2025-03-23 20:20:14 +0100

    testing

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "set a default payment processor" do
+    user = users(:one)
+    organization = Organization.create!(name: "Test", owner: user)
+    assert_equal "stripe", organization.payment_processor.processor
+  end
 end
