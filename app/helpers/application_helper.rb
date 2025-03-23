9bc26e078ef5af28ce8adfc9f766efd2225ead0f
Yaro Shm <yshmarov@gmail.com>
2025-03-23 16:22:47 +0100

    nav_link better api

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,14 @@ module ApplicationHelper
     end
   end
 
-  def nav_link(path, text, icon, **options)
+  def nav_link(label, path, icon:, **options)
     icon = inline_svg_tag icon, class: "size-5" if icon.match?(/svg/)
     active_link_to path, class_active: "bg-gray-200", class: "w-full items-center btn btn-transparent", **options do
       content_tag(:div) do
         icon
       end +
       content_tag(:div) do
-        text
+        label
       end
     end
   end
