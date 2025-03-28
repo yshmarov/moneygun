05baee265105b0775910784c4e0f62260404d0e5
Yaro Shm <yshmarov@gmail.com>
2025-03-29 00:01:02 +0100

    wip

--- app/controllers/organizations/credits_controller.rb
+++ app/controllers/organizations/credits_controller.rb
@@ -5,8 +5,12 @@ class Organizations::CreditsController < Organizations::BaseController
   end
 
   def create
-    credit_pack = UsageCredits.find_credit_pack(:starter)
-    session = credit_pack.create_checkout_session(current_user)
-    redirect_to session.url
+    if params[:credit_pack_id].present?
+      credit_pack = UsageCredits.find_credit_pack(params[:credit_pack_id])
+      session = credit_pack.create_checkout_session(current_organization)
+    end
+    # works only for subscriptions
+    # session = credit_pack.create_checkout_session(current_organization, cancel_url: organization_credits_url(current_organization), success_url: organization_credits_url(current_organization))
+    redirect_to session.url, allow_other_host: true, status: :see_other
   end
 end
