9b2143b074c8abf6360cb4929fb726fd2ba245b3
Yaro Shm <yshmarov@gmail.com>
2025-04-03 11:39:41 +0200

    i18n - move strings to locales (#215)
    * i18n org
    
    * Update devise.en.yml
    
    * wip
    
    * wip
    
    * Update en.yml
    
    * Update en.yml
    
    * more
    
    * wip
    
    * wip
    
    * more i18n
    
    * wip
    
    * wip
    
    * Update en.yml
    
    * clean
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = t("shared.errors.not_authorized")
     redirect_to(request.referrer || root_path)
   end
 
--- app/controllers/organizations/memberships_controller.rb
+++ app/controllers/organizations/memberships_controller.rb
@@ -16,7 +16,7 @@ class Organizations::MembershipsController < Organizations::BaseController
     @form = MembershipInvitation.new(email: params.dig(:membership_invitation, :email), role: params.dig(:membership_invitation, :role), organization: @organization, inviter: current_user)
 
     if @form.save
-      redirect_to organization_memberships_path(@organization), notice: "#{@form.email} invited!"
+      redirect_to organization_memberships_path(@organization), notice: t(".success", email: @form.email)
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class Organizations::MembershipsController < Organizations::BaseController
 
   def update
     if @membership.update(membership_params)
-      redirect_to organization_memberships_path(@organization), notice: "User updated"
+      redirect_to organization_memberships_path(@organization), notice: t(".success")
     else
       render :edit, status: :unprocessable_entity
     end
@@ -36,12 +36,12 @@ class Organizations::MembershipsController < Organizations::BaseController
   def destroy
     if @membership.try_destroy
       if @membership.user == current_user
-        redirect_to organizations_path, notice: "You have left that organization"
+        redirect_to organizations_path, notice: t(".you_have_left_the_organization")
       else
-        redirect_to organization_memberships_path(@organization), notice: "User removed from organization"
+        redirect_to organization_memberships_path(@organization), notice: t(".user_removed_from_organization")
       end
     else
-      redirect_to organization_memberships_path(@organization), alert: "Failed to remove user from organization"
+      redirect_to organization_memberships_path(@organization), alert: t(".failed_to_remove_user_from_organization")
     end
   end
 
