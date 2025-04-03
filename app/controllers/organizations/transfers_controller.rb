dbe4e666f12f97cfd47237116deac76727f87214
Yaro Shm <yshmarov@gmail.com>
2025-04-03 11:09:04 +0200

    wip

--- app/controllers/organizations/transfers_controller.rb
+++ app/controllers/organizations/transfers_controller.rb
@@ -1,12 +1,11 @@
 class Organizations::TransfersController < Organizations::BaseController
   before_action :authorize_organization_owner!
-
   def show
   end
 
   def update
     if @organization.transfer_ownership(params[:user_id])
-      redirect_to @organization, notice: "Ownership transferred successfully."
+      redirect_to @organization, notice: t("organizations.transfers.update.success")
     else
       render :show, status: :unprocessable_entity
     end
