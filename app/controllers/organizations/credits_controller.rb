f9773cc48c27ed77b65e48e4f2546236cd703269
Yaro Shm <yshmarov@gmail.com>
2025-04-05 02:37:43 +0200

    Update credits_controller.rb

--- app/controllers/organizations/credits_controller.rb
+++ app/controllers/organizations/credits_controller.rb
@@ -5,11 +5,11 @@ class Organizations::CreditsController < Organizations::BaseController
   def create
     if params[:credit_pack_id].present?
       credit_pack = UsageCredits.find_credit_pack(params[:credit_pack_id])
-      session = credit_pack.create_checkout_session(current_organization)
+      session = credit_pack.create_checkout_session(@organization)
     end
     if params[:subscription_plan_id].present?
       subscription_plan = UsageCredits.find_subscription_plan(params[:subscription_plan_id])
-      session = subscription_plan.create_checkout_session(current_organization, cancel_url: organization_credits_url(current_organization), success_url: organization_credits_url(current_organization))
+      session = subscription_plan.create_checkout_session(@organization, cancel_url: organization_credits_url(@organization), success_url: organization_credits_url(@organization))
     end
     redirect_to session.url, allow_other_host: true, status: :see_other
   end
