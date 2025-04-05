b1e5d5a12b3f0e079c03b31ecdfdb88b66766302
Yaro Shm <yshmarov@gmail.com>
2025-04-05 23:46:28 +0200

    Update credits_badge_component.rb

--- app/components/credits_badge_component.rb
+++ app/components/credits_badge_component.rb
@@ -9,7 +9,7 @@ class CreditsBadgeComponent < ViewComponent::Base
   end
 
   def wrapper_classes
-    default_classes = "font-medium border border-gray-200 bg-amber-400 dark:bg-amber-600 rounded-lg px-1"
+    default_classes = "font-extrabold border border-gray-200 bg-amber-400 dark:bg-amber-600 text-gray-900 dark:text-gray-100 rounded-lg px-1"
     default_classes += " hover:bg-amber-500 dark:hover:bg-amber-700 active:scale-95 transition-all duration-200" if link
     default_classes
   end
