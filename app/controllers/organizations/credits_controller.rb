371714b7c79b1e4c4e2ffc7ba776fdccc851efee
Yaro Shm <yshmarov@gmail.com>
2025-04-05 10:16:46 +0200

    Update credits_controller.rb

--- app/controllers/organizations/credits_controller.rb
+++ app/controllers/organizations/credits_controller.rb
@@ -4,13 +4,22 @@ class Organizations::CreditsController < Organizations::BaseController
 
   def create
     if params[:credit_pack_id].present?
-      credit_pack = UsageCredits.find_credit_pack(params[:credit_pack_id])
-      session = credit_pack.create_checkout_session(@organization)
-    end
-    if params[:subscription_plan_id].present?
-      subscription_plan = UsageCredits.find_subscription_plan(params[:subscription_plan_id])
-      session = subscription_plan.create_checkout_session(@organization, cancel_url: organization_credits_url(@organization), success_url: organization_credits_url(@organization))
+      handle_credit_pack_purchase
+    elsif params[:subscription_plan_id].present?
+      handle_subscription_plan_purchase
     end
     redirect_to session.url, allow_other_host: true, status: :see_other
+  end
+
+  private
+
+  def handle_credit_pack_purchase
+    credit_pack = UsageCredits.find_credit_pack(params[:credit_pack_id])
+    session = credit_pack.create_checkout_session(@organization)
+  end
+
+  def handle_subscription_plan_purchase
+    subscription_plan = UsageCredits.find_subscription_plan(params[:subscription_plan_id])
+    session = subscription_plan.create_checkout_session(@organization, cancel_url: organization_credits_url(@organization), success_url: organization_credits_url(@organization))
   end
 end
