66ae13c70d2643afe3b597997d4726e9bc421fcc
Yaro Shm <yshmarov@gmail.com>
2025-04-09 14:59:16 +0200

    add solid cache db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_09_120227) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_09_125851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -165,6 +165,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_09_120227) do
     t.datetime "updated_at", null: false
     t.index ["name", "organization_id"], name: "index_projects_on_name_and_organization_id", unique: true
     t.index ["organization_id"], name: "index_projects_on_organization_id"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
