38e3fa26c60a6649de9bc079b2480c416f455581
Yaro Shm <yshmarov@gmail.com>
2025-04-09 13:47:17 +0200

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
   # config.active_job.queue_adapter = :inline
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  # config.solid_queue.connects_to = { database: { writing: :queue } }
   # config.solid_queue.silence_polling = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
