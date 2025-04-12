52064c670c64bec131a8af1eccf439c05d64b911
Yaro Shm <yshmarov@gmail.com>
2025-04-13 00:49:32 +0200

    avatars: similar colors and text

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -3,7 +3,7 @@ module OrganizationsHelper
     if organization.logo.attached?
       image_tag organization.logo, class: "rounded size-8 object-contain", alt: organization.name
     else
-      tag.div organization.name[0..1], class: "size-8 uppercase rounded bg-gray-600 text-white text-xs flex items-center justify-center"
+      tag.div organization.name[0..1], class: "size-8 uppercase rounded bg-gray-400 text-xs flex items-center justify-center"
     end
   end
 end
