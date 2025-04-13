962f79bf73309885cb879bf8d2a830bf6a076ba0
Yaro Shm <yshmarov@gmail.com>
2025-04-13 14:53:46 +0200

    more cleanup

--- app/jobs/hello_world_job.rb
+++
@@ -1,8 +0,0 @@
-class HelloWorldJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    puts "Hello, world!"
-    # Do something later
-  end
-end
--- test/jobs/hello_world_job_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class HelloWorldJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
