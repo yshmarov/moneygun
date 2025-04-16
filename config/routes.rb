30d2c707c3d92e89bce3c9d13b38eb84e433728e
Yaro Shm <yshmarov@gmail.com>
2025-04-16 12:47:43 +0200

    i18n routes for memberships

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :organizations, path: I18n.t("routes.organizations") do
     scope module: :organizations do
-      resources :memberships, except: %i[show]
+      resources :memberships, except: %i[show], path: I18n.t("routes.memberships")
       resource :transfer, only: %i[show update]
       resources :projects
 
