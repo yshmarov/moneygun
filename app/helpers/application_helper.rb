fb552d2c7f66f983a60e6904b63669301711df2d
Yaro Shm <yshmarov@gmail.com>
2025-05-03 12:52:18 +0200

    du alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def flash_style(type)
     case type
-    when "notice" then "bg-green-100 border-green-400 text-green-700"
-    when "alert" then "bg-red-100 border-red-400 text-red-700"
+    when "notice" then "du-alert-info"
+    when "alert" then "du-alert-error"
     end
   end
 
