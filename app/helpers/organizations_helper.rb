d393f4aad4e608a4a87b7b5591aed280429ad88a
Yaro Shm <yshmarov@gmail.com>
2025-05-03 14:34:22 +0200

    Add Daisy UI; Apply some basic classes and components (#234)
    * install daisy ui
    
    * apply buttons
    
    * delete custom themes
    
    * default theme cupcake
    
    * layout bg
    
    * use more active btn classes
    
    * Update _sidebar.html.erb
    
    * Update _sidebar_2_cols.html.erb
    
    * dock (footer nav)
    
    * Update _organization.html.erb
    
    * du alerts
    
    * org avatar dynamic img size

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,9 +1,9 @@
 module OrganizationsHelper
-  def organization_avatar(organization)
+  def organization_avatar(organization, classes: "size-8")
     if organization.logo.attached?
-      image_tag organization.logo, class: "rounded size-8 object-contain", alt: organization.name
+      image_tag organization.logo, class: "rounded #{classes} object-contain", alt: organization.name
     else
-      tag.div organization.name[0..1], class: "size-8 uppercase rounded bg-gray-400 text-xs flex items-center justify-center"
+      tag.div organization.name[0..1], class: "uppercase rounded bg-gray-400 text-xs flex items-center justify-center #{classes}"
     end
   end
 end
