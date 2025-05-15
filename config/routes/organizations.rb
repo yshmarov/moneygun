763c3bf09c2655b7953617d9c3047f2f2e954a69
Natan Nobre Chaves <natannobre37@gmail.com>
2025-05-15 13:03:45 -0300

    Add membership request management functionality (#241)
    - Introduced Organizations::MembershipRequestsController for handling membership requests, including approval and rejection actions.
    - Created Users::MembershipRequestsController for users to submit membership requests to organizations.
    - Added MembershipRequest model to manage request logic and validations.
    - Updated views for displaying and managing membership requests, including new index and user request forms.

--- config/routes/organizations.rb
+++ config/routes/organizations.rb
@@ -5,6 +5,12 @@ resources :organizations, path: I18n.t("routes.organizations") do
     resources :memberships, except: %i[show], path: I18n.t("routes.memberships")
     resource :transfer, only: %i[show update]
     resources :invitations, only: %i[index destroy]
+    resources :membership_requests, only: %i[index] do
+      member do
+        post :approve
+        post :reject
+      end
+    end
 
     get "subscriptions", to: "subscriptions#index"
     get "subscriptions/checkout", to: "subscriptions#checkout"
