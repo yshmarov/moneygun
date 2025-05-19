e538e568f33ae77e1cac0dd02076a09a2b66825a
Yaro Shm <yshmarov@gmail.com>
2025-05-19 19:35:03 +0200

    wip

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -13,19 +13,22 @@ module OrganizationsHelper
       {
         display_text: "Invite only",
         description_text: "People can join your server directly with an invite",
-        icon_path: "svg/lock.svg"
+        # icon_path: "svg/lock.svg"
+        icon_path: "🔐"
       }
     when "restricted"
       {
         display_text: "Apply to join",
         description_text: "People must submit an application to be approved to join",
-        icon_path: "svg/envelope.svg"
+        # icon_path: "svg/envelope.svg"
+        icon_path: "📨"
       }
     when "public"
       {
         display_text: "Discoverable",
         description_text: "Anyone can join your server directly through Server Discovery",
-        icon_path: "svg/globe.svg"
+        # icon_path: "svg/globe.svg"
+        icon_path: "🌐"
       }
     else
       # Default or error handling
