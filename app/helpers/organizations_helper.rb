ed312a13b1074f9874b441e84194a944d6409cb9
Yaro Shm <yshmarov@gmail.com>
2025-05-19 20:28:33 +0200

    org logo variants, image_processing

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,7 +1,7 @@
 module OrganizationsHelper
   def organization_avatar(organization, classes: "size-8")
     if organization.logo.attached?
-      image_tag organization.logo, class: "rounded #{classes} object-contain", alt: organization.name
+      image_tag organization.logo.variant(:thumb), class: "rounded #{classes} object-contain", alt: organization.name
     else
       tag.div organization.name[0..1], class: "uppercase rounded bg-gray-400 text-xs flex items-center justify-center #{classes}"
     end
