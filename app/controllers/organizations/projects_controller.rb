7b125520990704e5dc4d9350dfc3081e00d2ddb9
Yaro Shm <yshmarov@gmail.com>
2025-05-19 22:30:01 +0200

    project edit modal

--- app/controllers/organizations/projects_controller.rb
+++ app/controllers/organizations/projects_controller.rb
@@ -28,7 +28,8 @@ class Organizations::ProjectsController < Organizations::BaseController
 
     if @project.save
       respond_to do |format|
-        format.html { redirect_to organization_project_url(@organization, @project), notice: "Project was successfully created." }
+        flash[:notice] = "Project was successfully created."
+        format.html { redirect_to organization_project_url(@organization, @project) }
         format.turbo_stream { render turbo_stream: turbo_stream.redirect_to(organization_project_url(@organization, @project)) }
       end
     else
@@ -39,7 +40,11 @@ class Organizations::ProjectsController < Organizations::BaseController
   # PATCH/PUT /organizations/1/projects/1
   def update
     if @project.update(project_params)
-      redirect_to organization_project_url(@organization, @project), notice: "Project was successfully updated."
+      respond_to do |format|
+        flash[:notice] = "Project was successfully updated."
+        format.html { redirect_to organization_project_url(@organization, @project) }
+        format.turbo_stream { render turbo_stream: turbo_stream.redirect_to(organization_project_url(@organization, @project)) }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
