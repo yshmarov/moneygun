24448ec43408938aac4c97524999bf9868e629fa
Yaro Shm <yshmarov@gmail.com>
2025-05-25 09:15:37 +0200

    rm "content_container" - not used enough

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -1,21 +1,12 @@
 class PageComponent < ViewComponent::Base
   renders_one :action_list
 
-  def initialize(title:, content_container: false, full_width: false)
+  def initialize(title:, full_width: false)
     @title = title
-    @content_container = content_container
     @full_width = full_width
   end
 
   attr_reader :title
-
-  def content_container_class
-    default_classes = "space-y-4"
-    container_classes = "border rounded-lg p-4 shadow"
-    return [ default_classes, container_classes ].join(" ") if @content_container == true
-
-    default_classes
-  end
 
   def width_class
     if @full_width == true
