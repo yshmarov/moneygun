b3f31ee0d9f59fe116a55826b6739d5acddfb289
Yaro Shm <yshmarov@gmail.com>
2025-05-25 17:53:14 +0200

    Add Avo admin (#157)
    * add admin to users
    
    * run avo template generator
    
    bin/rails app:template LOCATION='https://avohq.io/app-template'
    
    * generate other avo admin resources
    
    rails generate avo:all_resources
    
    rails generate avo:resource organization
    rails generate avo:resource membership
    
    * improve fields
    
    * admin auth
    
    * search, sort, format date, titles
    
    * attach scopes do not include duplicates
    
    * lint
    
    * avo tests
    
    * bundle
    
    * cleanup
    
    * cleanup
    
    * clean
    
    * Update profitable_controller_test.rb
    
    * add admin nav

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -1,4 +1,5 @@
 authenticate :user, ->(user) { user.admin? } do
   mount Profitable::Engine => "/profitable"
   mount MissionControl::Jobs::Engine, at: "/jobs"
+  mount Avo::Engine, at: Avo.configuration.root_path
 end
---
+++ test/controllers/avo_auth_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class AvoAuthTest < ActionDispatch::IntegrationTest
+  test "admin user can access avo" do
+    user = users(:one)
+    sign_in user
+    get Avo.configuration.root_path
+    assert_redirected_to "/admin/avo/resources/users"
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "non-admin user cannot access avo" do
+    user = users(:two)
+    sign_in user
+    get Avo.configuration.root_path
+    assert_response :not_found
+  end
+end
