f1e49f943911239844d7434e06253ceb00b9d5d7
Yaro Shm <yshmarov@gmail.com>
2025-05-26 11:02:24 +0200

    Global search (organizations) (#256)
    * global search
    
    * cmd k
    
    * search style
    
    * Update _navbar.html.erb
    
    * Update _navbar.html.erb
    
    * Update index.html.erb
    
    * Update index.html.erb
    
    * Update index.html.erb
    
    * Update index.html.erb
    
    * fix modal to top
    
    * Update _modal.html.erb
    
    * i18n
    
    * Update dialog_controller.js
    
    * cleaner
    
    * test
    
    * debounce

---
+++ app/javascript/controllers/debounce_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  submit(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    setTimeout(() => {
+      this.element.requestSubmit()
+    }, 300)
+  }
+}
