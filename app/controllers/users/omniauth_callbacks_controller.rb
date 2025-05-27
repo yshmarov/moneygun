5cddc03a26383aa708f02ab41bfd28c34cfafeb4
Yaro Shm <yshmarov@gmail.com>
2025-05-27 20:18:13 +0200

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,11 +15,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect user, event: :authentication
     else
       session["devise.auth_data"] = request.env["omniauth.auth"].except(:extra)
-      redirect_to root_path, alert: user.errors.full_messages.join("\n")
+      redirect_to new_user_registration_url, alert: user.errors.full_messages.join("\n")
     end
   end
 
   def failure
-    redirect_to root_path, alert: "Something went wrong. Please try again"
+    redirect_to new_user_registration_url, alert: "Something went wrong. Please try again"
   end
 end
