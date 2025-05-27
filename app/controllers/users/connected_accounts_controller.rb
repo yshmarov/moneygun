158d0c8745f2ddd11535892dce67be08cf4bca74
Yaro Shm <yshmarov@gmail.com>
2025-05-27 14:26:48 +0200

    vibe style

--- app/controllers/users/connected_accounts_controller.rb
+++ app/controllers/users/connected_accounts_controller.rb
@@ -1,5 +1,18 @@
 class Users::ConnectedAccountsController < ApplicationController
+  before_action :set_connected_account, only: [ :destroy ]
+
   def index
     @connected_accounts = current_user.connected_accounts
+  end
+
+  def destroy
+    @connected_account.destroy
+    redirect_to user_connected_accounts_path, notice: "Account disconnected successfully."
+  end
+
+  private
+
+  def set_connected_account
+    @connected_account = current_user.connected_accounts.find(params[:id])
   end
 end
