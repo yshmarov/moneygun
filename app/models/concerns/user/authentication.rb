8446d84da347dafe52e2474d298fa4dc7a56f7a2
Yaro Shm <yshmarov@gmail.com>
2025-05-27 11:32:35 +0200

    google_onetap mvp

--- app/models/concerns/user/authentication.rb
+++ app/models/concerns/user/authentication.rb
@@ -10,6 +10,20 @@ module User::Authentication
   end
 
   class_methods do
+    def from_google_onetap(payload)
+      user = User.where(email: payload["email"]).first_or_initialize do |user|
+        user.email = payload["email"]
+        user.password = Devise.friendly_token[0, 20] if user.password.blank?
+      end
+
+      # user.name = payload["name"]
+      # user.image = payload["picture"]
+      # user.provider = "google_oauth2"
+      # user.uid = payload["sub"]
+      user.save
+      user
+    end
+
     def from_omniauth(auth_payload)
       data = auth_payload.info
       user = User.where(email: data["email"]).first_or_initialize do |user|
