df964eb483bf7ee8eb08e83e5dfa308f77f4178b
Yaro Shm <yshmarov@gmail.com>
2025-05-31 10:26:37 +0200

    wip

--- app/models/connected_account.rb
+++ app/models/connected_account.rb
@@ -1,3 +1,10 @@
 class ConnectedAccount < ApplicationRecord
   belongs_to :user
+
+  PROVIDER_CONFIG = {
+    google_oauth2: { name: "Google", icon: "🔍" },
+    github: { name: "GitHub", icon: "🐙" },
+    facebook: { name: "Facebook", icon: "📘" },
+    twitter: { name: "Twitter", icon: "🐦" }
+  }.freeze
 end
