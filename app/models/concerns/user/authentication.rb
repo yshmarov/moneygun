0dbac92e27a48212626f7c2b0574186843ab2dfe
Yaro Shm <yshmarov@gmail.com>
2025-05-31 10:59:51 +0200

    Update authentication.rb

--- app/models/concerns/user/authentication.rb
+++ app/models/concerns/user/authentication.rb
@@ -12,28 +12,28 @@ module User::Authentication
   end
 
   class_methods do
-    def from_google_onetap(payload)
-      user = User.where(email: payload["email"]).first_or_initialize do |user|
-        user.email = payload["email"]
-        user.password = Devise.friendly_token[0, 20] if user.password.blank?
-      end
-
+    def from_google_onetap(onetap_payload)
+      user = find_or_initialize_by_email(onetap_payload["email"])
       user.save
 
       if user.persisted?
-        ConnectedAccount.create_or_update_from_omniauth(payload, user)
+        auth_hash = adapt_onetap_payload_to_auth_hash(onetap_payload, "google_oauth2")
+        ConnectedAccount.create_or_update_from_omniauth(auth_hash, user)
       end
 
       user
     end
 
     def from_omniauth(auth_payload)
-      data = auth_payload.info
-      user = User.where(email: data["email"]).first_or_initialize do |user|
-        user.email = data["email"]
-        user.password = Devise.friendly_token[0, 20] if user.password.blank?
+      email = auth_payload.info&.email
+      email ||= auth_payload.uid if auth_payload.provider == "saml"
+
+      unless email
+        Rails.logger.error "OmniAuth: Email not found in payload for provider #{auth_payload.provider}"
+        return nil
       end
 
+      user = find_or_initialize_by_email(email)
       user.save
 
       if user.persisted?
@@ -41,6 +41,31 @@ module User::Authentication
       end
 
       user
+    end
+
+    private
+
+    def find_or_initialize_by_email(email)
+      User.where(email: email).first_or_initialize do |user|
+        user.email = email
+        user.password = Devise.friendly_token[0, 20] if user.password.blank?
+      end
+    end
+
+    def adapt_onetap_payload_to_auth_hash(onetap_payload, provider_name)
+      OmniAuth::AuthHash.new({
+        provider: provider_name,
+        uid: onetap_payload["sub"], # 'sub' is typically the user ID in Google's token
+        info: {
+          email: onetap_payload["email"],
+          name: onetap_payload["name"],
+          image: onetap_payload["picture"]
+        },
+        credentials: {}
+        # OneTap doesn't directly provide access/refresh tokens in the same way as a full OAuth2 flow.
+        # If you have separate mechanisms to get these for OneTap, they could be added here.
+        # For now, credentials will be empty.
+      })
     end
   end
 end
