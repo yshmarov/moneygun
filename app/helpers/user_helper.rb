b21923004c5f752e82c7c12d91bd834fcec350b9
Yaro Shm <yshmarov@gmail.com>
2025-05-31 12:15:02 +0200

    w

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,8 +1,12 @@
 module UserHelper
   def user_avatar(user)
-    tag.div class: "du-avatar du-avatar-placeholder" do
-      tag.div class: "bg-neutral text-neutral-content w-8 rounded-full" do
-        tag.span user.email[0..1].upcase, class: "text-xs"
+    if user.connected_accounts.any?
+      image_tag user.connected_accounts.first.image_url, alt: user.connected_accounts.first.name, class: "w-8 h-8 rounded-full"
+    else
+      tag.div class: "du-avatar du-avatar-placeholder" do
+        tag.div class: "bg-neutral text-neutral-content w-8 rounded-full" do
+          tag.span user.email[0..1].upcase, class: "text-xs"
+        end
       end
     end
   end
