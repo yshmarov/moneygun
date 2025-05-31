e1296a67da46595130bee0a9fb6739ea7de4962f
Yaro Shm <yshmarov@gmail.com>
2025-05-31 10:55:05 +0200

    cleaner

--- app/models/concerns/user/authentication.rb
+++ app/models/concerns/user/authentication.rb
@@ -31,20 +31,9 @@ module User::Authentication
 
       user.save
 
-      connected_account = user.connected_accounts.find_or_initialize_by(
-        provider: auth_payload.provider,
-        uid: auth_payload.uid
-      )
-      connected_account.payload = auth_payload.to_h
-
-      credentials = auth_payload.credentials
-      if credentials.present?
-        connected_account.access_token = credentials.token
-        connected_account.refresh_token = credentials.refresh_token
-        connected_account.expires_at = Time.at(credentials.expires_at) if credentials.expires_at.present?
+      if user.persisted?
+        ConnectedAccount.create_or_update_from_omniauth(auth_payload, user)
       end
-
-      connected_account.save
 
       user
     end
--- app/models/connected_account.rb
+++ app/models/connected_account.rb
@@ -19,4 +19,23 @@ class ConnectedAccount < ApplicationRecord
   def image_url
     payload&.dig("info", "image")
   end
+
+  def self.create_or_update_from_omniauth(auth_payload, user)
+    connected_account = user.connected_accounts.find_or_initialize_by(
+      provider: auth_payload.provider,
+      uid: auth_payload.uid
+    )
+
+    connected_account.payload = auth_payload.to_h
+
+    credentials = auth_payload.credentials
+    if credentials.present?
+      connected_account.access_token = credentials.token
+      connected_account.refresh_token = credentials.refresh_token
+      connected_account.expires_at = Time.at(credentials.expires_at) if credentials.expires_at.present?
+    end
+
+    connected_account.save
+    connected_account
+  end
 end
