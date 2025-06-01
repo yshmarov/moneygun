16ed4cf4bfe5390c6ee42b6473562c6136048571
Yaro Shm <yshmarov@gmail.com>
2025-06-01 23:06:52 +0200

    set i18n basics for app

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module Moneygun
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.mission_control.jobs.http_basic_auth_enabled = false
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = %i[en]
+    config.i18n.fallbacks = true
   end
 end
