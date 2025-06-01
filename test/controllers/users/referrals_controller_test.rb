099f3ed95dc6b80b12ef1f73e5ee77a32fd6c38c
Yaro Shm <yshmarov@gmail.com>
2025-06-01 20:42:54 +0200

    test

---
+++ test/controllers/users/referrals_controller_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class Users::ReferralsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
+  test "get #index" do
+    assert_difference "Refer::ReferralCode.count" do
+      get user_referrals_url
+      assert_response :success
+    end
+
+    assert_no_difference "Refer::ReferralCode.count" do
+      get user_referrals_url
+      assert_response :success
+    end
+  end
+end
