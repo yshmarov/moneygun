ca71355d159e20045990f0f838cc05d7e8d36d6d
Yaro Shm <yshmarov@gmail.com>
2025-06-01 23:07:01 +0200

    authentication concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,11 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :authenticate_user!
   before_action :set_current_organizations, if: :user_signed_in?
 
   set_referral_cookie
 
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || organizations_path
-  end
-
+  include Authentication
   include Authorization
 
   def current_organization
