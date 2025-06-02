80ae5f0db7ca596b59384d0b300997b4ff8ef042
Yaro Shm <yshmarov@gmail.com>
2025-06-02 10:59:31 +0200

    I18n tasks better setup (#265)
    * i18n tasks better setup
    
    * clean up some unused keys
    
    * ignore some keys
    
    * fix more keys

--- test/i18n_test.rb
+++ test/i18n_test.rb
@@ -6,14 +6,12 @@ class I18nTest < ActiveSupport::TestCase
   end
 
   def test_no_missing_keys
-    skip
     missing_keys = @i18n.missing_keys
     assert_empty missing_keys,
                  "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
 
   def test_no_unused_keys
-    skip
     unused_keys = @i18n.unused_keys
     assert_empty unused_keys,
                  "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
