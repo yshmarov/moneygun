0f70ae3cdaf02ababac50ebabb68aef648d0bfd8
Yaro Shm <yshmarov@gmail.com>
2025-06-04 16:13:43 +0200

    Update static_helper.rb

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,7 +1,22 @@
 module StaticHelper
   def markdown_to_html(markdown_text)
     renderer = Redcarpet::Render::HTML.new
-    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    extensions = {
+      hard_wrap: true,
+      autolink: true,
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true,
+      strikethrough: true,
+      lax_spacing: true,
+      space_after_headers: true,
+      quote: true,
+      footnotes: true,
+      highlight: true,
+      underline: true
+    }
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(markdown_text).html_safe
   end
 end
