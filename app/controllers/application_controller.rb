81f5e7bc79f76f9e00eced0ffab74566770f3336
Yaro Shm <yshmarov@gmail.com>
2025-06-06 08:42:22 +0200

    simplify def layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
 
-  layout :determine_layout
-
   before_action :set_current_organizations, if: :user_signed_in?
 
   set_referral_cookie
@@ -20,11 +18,5 @@ class ApplicationController < ActionController::Base
 
   def set_current_organizations
     Current.organizations = current_user.organizations
-  end
-
-  private
-
-  def determine_layout
-    user_signed_in? ? "application" : "marketing"
   end
 end
