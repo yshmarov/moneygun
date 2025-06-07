ac4d6ba5a8fc5318a7c1fe00a32fa91be8fe0058
Yaro Shm <yshmarov@gmail.com>
2025-06-07 12:56:19 +0200

    stimulus-animated-number
    https://www.stimulus-components.com/docs/stimulus-animated-number

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "turbo_power" # @0.7.1
 pin "@stimulus-components/clipboard", to: "@stimulus-components--clipboard.js" # @5.0.0
+pin "autonumeric", to: "https://ga.jspm.io/npm:autonumeric@4.6.0/dist/autoNumeric.min.js"
+pin "@stimulus-components/animated-number", to: "@stimulus-components--animated-number.js" # @5.0.0
