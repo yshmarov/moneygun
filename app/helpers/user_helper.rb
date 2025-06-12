ce0b9b08b768b45e89fcde0c2d2b5ccdf03f413f
Yaro Shm <yshmarov@gmail.com>
2025-06-12 15:08:33 +0200

    better avatar group

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,10 +3,8 @@ module UserHelper
     if user.connected_accounts.any? && user.connected_accounts.first.image_url.present?
       image_tag user.connected_accounts.first.image_url, alt: user.connected_accounts.first.name, class: "w-8 h-8 rounded-full"
     else
-      tag.div class: "du-avatar du-avatar-placeholder" do
-        tag.div class: "bg-neutral text-neutral-content w-8 rounded-full" do
-          tag.span user.email[0..1].upcase, class: "text-xs"
-        end
+      tag.div class: "aspect-square flex items-center justify-center bg-neutral text-neutral-content w-8 rounded-full" do
+        tag.span user.email[0..1].upcase, class: "text-xs"
       end
     end
   end
