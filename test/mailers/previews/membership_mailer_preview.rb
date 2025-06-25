21447846bd1883003946063d86c13fd2a8c24ead
Yaro Shm <yshmarov@gmail.com>
2025-06-25 12:03:42 +0200

    Update membership_mailer_preview.rb

--- test/mailers/previews/membership_mailer_preview.rb
+++ test/mailers/previews/membership_mailer_preview.rb
@@ -1,3 +1,22 @@
 # Preview all emails at http://localhost:3000/rails/mailers/membership_mailer
 class MembershipMailerPreview < ActionMailer::Preview
+  def invitation_email
+    organization = Organization.first
+    user = User.first
+
+    MembershipInvitationNotifier.with(organization: organization).deliver(user)
+    notification = user.notifications.last
+
+    MembershipMailer.invitation_email(notification)
+  end
+
+  def request_accepted_email
+    organization = Organization.first
+    user = User.first
+
+    MembershipRequestAcceptedNotifier.with(organization: organization).deliver(user)
+    notification = user.notifications.last
+
+    MembershipMailer.request_accepted_email(notification)
+  end
 end
