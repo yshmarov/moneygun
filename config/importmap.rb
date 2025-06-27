f31c8b21ddface9ff01447b2a979bd6c11026284
Yaro Shm <yshmarov@gmail.com>
2025-06-27 21:38:44 +0200

    @stimulus-components/auto-submit

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "turbo_power" # @0.7.1
 pin "@stimulus-components/clipboard", to: "@stimulus-components--clipboard.js" # @5.0.0
 pin "autonumeric", to: "https://ga.jspm.io/npm:autonumeric@4.6.0/dist/autoNumeric.min.js"
 pin "@stimulus-components/animated-number", to: "@stimulus-components--animated-number.js" # @5.0.0
+pin "@stimulus-components/auto-submit", to: "@stimulus-components--auto-submit.js" # @6.0.0
