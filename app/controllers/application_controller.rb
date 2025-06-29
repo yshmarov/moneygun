f71eddc4d57a3120e0e34b1ce5224b4f6b6a975a
Yaro Shm <yshmarov@gmail.com>
2025-06-29 23:49:02 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
 
-  include Pagy::Backend
 
   before_action :set_current_organizations, if: :user_signed_in?
 
   set_referral_cookie
 
+  include Pagy::Backend
   include Authentication
   include Authorization
   include Analytics
