ca49fcd984a2dec2c8e79cff7562d77088a2a3a9
Yaro Shm <yshmarov@gmail.com>
2025-08-24 16:09:12 +0200

    bundle update; normalize i18n; fix tests

--- app/models/connected_account.rb
+++ app/models/connected_account.rb
@@ -4,7 +4,7 @@ class ConnectedAccount < ApplicationRecord
   validates :provider, presence: true
   validates :uid, presence: true, uniqueness: { scope: :provider }
 
-  encrypts :access_token, :refresh_token
+  encrypts :access_token, :refresh_token if Rails.application.credentials.active_record_encryption.present?
 
   PROVIDER_CONFIG = {
     google_oauth2: {
