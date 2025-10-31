c0d07b1c2976398feb1046ef1ad669dfbbf39b74
Yaro Shm <yshmarov@gmail.com>
2025-11-01 00:05:17 +0100

    simplify notifications in nav

--- app/models/concerns/noticed/notification_extensions.rb
+++ app/models/concerns/noticed/notification_extensions.rb
@@ -4,20 +4,20 @@ module Noticed::NotificationExtensions
   extend ActiveSupport::Concern
 
   def broadcast_replace_to_index_count
-    broadcast_replace_to(
+    Turbo::StreamsChannel.broadcast_replace_to(
       "notifications_index_#{recipient.id}",
       target: "notification_index_count",
       partial: "users/notifications/notifications_count",
-      locals: {unread: recipient.reload.unseen_notifications_count}
+      locals: { unread: recipient.reload.unseen_notifications_count }
     )
   end
 
   def broadcast_prepend_to_index_list
-    broadcast_prepend_to(
+    Turbo::StreamsChannel.broadcast_prepend_to(
       "notifications_index_list_#{recipient.id}",
       target: "notifications",
       partial: "users/notifications/notification",
-      locals: {notification: self}
+      locals: { notification: self }
     )
   end
 end
