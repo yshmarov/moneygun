c7fca8f9160a769c61695f951418a84dce849ba4
Yaro Shm <yshmarov@gmail.com>
2025-11-01 15:48:41 +0100

    noticed notifications - fix turbo_stream delivery method

--- app/models/concerns/noticed/notification_extensions.rb
+++ app/models/concerns/noticed/notification_extensions.rb
@@ -3,20 +3,20 @@
 module Noticed::NotificationExtensions
   extend ActiveSupport::Concern
 
-  def broadcast_replace_to_index_count
-    Turbo::StreamsChannel.broadcast_replace_to(
+  def broadcast_update_to_bell
+    broadcast_update_to(
       "notifications_#{recipient.id}",
-      target: "notification_count",
-      partial: "users/notifications/notifications_count",
-      locals: { unread: recipient.reload.unseen_notifications_count }
+      target: "notification_bell",
+      partial: "navbar/notifications/bell",
+      locals: { user: recipient }
     )
   end
 
   def broadcast_prepend_to_index_list
-    Turbo::StreamsChannel.broadcast_prepend_to(
-      "notifications_#{recipient.id}",
+    broadcast_prepend_to(
+      "notifications_index_list_#{recipient.id}",
       target: "notifications",
-      partial: "users/notifications/notification",
+      partial: "notifications/notification",
       locals: { notification: self }
     )
   end
--- app/notifiers/delivery_methods/turbo_stream.rb
+++ app/notifiers/delivery_methods/turbo_stream.rb
@@ -4,7 +4,7 @@ class DeliveryMethods::TurboStream < ApplicationDeliveryMethod
   def deliver
     return unless recipient.is_a?(User)
 
-    notification.broadcast_replace_to_index_count
+    notification.broadcast_update_to_bell
     notification.broadcast_prepend_to_index_list
   end
 end
