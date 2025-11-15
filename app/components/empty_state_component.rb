a438c75b52de24241ed6aca28799c8cb09b3e804
Yaro Shm <yshmarov@gmail.com>
2025-11-15 13:33:06 +0100

    update EmptyStateComponent to use new icon def

--- app/components/empty_state_component.rb
+++ app/components/empty_state_component.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# <%= render EmptyStateComponent.new(title: "No submissions yet") do |c| %>
-#   <% c.with_icon do %>
-#     <%= inline_svg_tag "svg/home.svg", class: "w-10 h-10 text-base-content/50" %>
-#   <% end %>
-# <% end %>
 class EmptyStateComponent < ViewComponent::Base
   def initialize(title: "Nothing here yet", subtitle: "Come back soon", icon: nil)
     @title = title
