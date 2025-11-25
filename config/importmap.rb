d2845e9cfd04956e51954da356663a4a0fade23f
Yaro Shm <yshmarov@gmail.com>
2025-11-25 01:22:57 +0100

    dont preload rarely used external js dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,7 +9,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "turbo_power" # @0.7.1
 pin "@stimulus-components/clipboard", to: "@stimulus-components--clipboard.js" # @5.0.0
-pin "autonumeric", to: "https://ga.jspm.io/npm:autonumeric@4.6.0/dist/autoNumeric.min.js"
-pin "@stimulus-components/animated-number", to: "@stimulus-components--animated-number.js" # @5.0.0
+pin "autonumeric", to: "https://ga.jspm.io/npm:autonumeric@4.6.0/dist/autoNumeric.min.js", preload: false
+pin "@stimulus-components/animated-number", to: "@stimulus-components--animated-number.js", preload: false # @5.0.0
 pin "@stimulus-components/auto-submit", to: "@stimulus-components--auto-submit.js" # @6.0.0
 pin "local-time" # @3.0.3
