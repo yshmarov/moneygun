d67ac3d055fc8bacdad879982669cf4f549c8e12
Yaro Shm <yshmarov@gmail.com>
2025-11-29 14:34:43 +0100

    Enable and configure 'remember me' for OAuth sign-in
    Sets 'remember: true' when signing in users via Omniauth and enables Devise's extend_remember_period. Configures rememberable cookie options for improved security, including secure, httponly, and same_site settings.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -32,7 +32,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           refer user
         end
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: ConnectedAccount::PROVIDER_CONFIG[kind][:name]
-        sign_in_and_redirect user, event: :authentication
+        sign_in_and_redirect user, event: :authentication, remember: true
       else
         session["devise.auth_data"] = auth_payload.except(:extra)
         redirect_to new_user_registration_url, alert: user.errors.full_messages.join("\n")
