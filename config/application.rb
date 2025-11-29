500950b10323ef00f29bbcacad1f50e1a1948760
Yaro Shm <yshmarov@gmail.com>
2025-11-29 08:37:02 +0100

    Move Lookbook preview_layout config to development env
    Relocated the Lookbook preview_layout setting from application.rb to development.rb to ensure it only applies in the development environment.

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module Moneygun
     config.i18n.default_locale = :en
     config.i18n.available_locales = %i[en fr]
     config.i18n.fallbacks = true
-    config.lookbook.preview_layout = "minimal"
     config.active_record.encryption.support_unencrypted_data = true
     config.active_record.encryption.extend_queries = true
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.hosts = nil
+  config.lookbook.preview_layout = "minimal"
 end
