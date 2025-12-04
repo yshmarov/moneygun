5a4f062b9cb1071f650b493fe776a670b8c37f39
Yaro Shm <yshmarov@gmail.com>
2025-12-04 23:31:14 +0100

    after sign in redirect to first org by default

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -11,7 +11,13 @@ module Authentication
   private
 
   def after_sign_in_path_for(resource)
-    if session[:new_user]
+    stored_location = stored_location_for(resource)
+    return stored_location if stored_location
+
+    if resource.organizations.any?
+      session.delete(:new_user) if session[:new_user]
+      organization_path(resource.organizations.first)
+    elsif session[:new_user]
       session.delete(:new_user)
       # You can send new users to onboarding, billing, or somewhere else
       root_path
---
+++ test/controllers/concerns/authentication_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class AuthenticationTest < ActionDispatch::IntegrationTest
+  setup do
+    @user_with_org = users(:one)
+    @organization = organizations(:one)
+  end
+
+  test "after_sign_in_path_for" do
+    user = User.create!(
+      email: "noorg@example.com",
+      password: "password123",
+      confirmed_at: Time.current
+    )
+
+    post user_session_path, params: { user: { email: user.email, password: user.password } }
+    organization = user.organizations.first
+    assert_redirected_to organization_path(organization)
+
+    organization.destroy
+    sign_out user
+
+    post user_session_path, params: { user: { email: user.email, password: user.password } }
+    assert_redirected_to root_path
+  end
+end
