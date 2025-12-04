24365ad56b05ac59621f82d2581955bf595c476a
Yaro Shm <yshmarov@gmail.com>
2025-12-04 23:59:14 +0100

    set remember_me in sessions_controller by default

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,4 +2,9 @@
 
 class Users::SessionsController < Devise::SessionsController
   rate_limit to: 50, within: 3.minutes, only: :create, with: -> { redirect_to new_user_session_url, alert: t("shared.errors.rate_limit") }
+
+  def create
+    params[:user][:remember_me] = "1"
+    super
+  end
 end
