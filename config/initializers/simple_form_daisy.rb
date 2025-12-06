b02c5b5343e1104e9df6d5b0b3209fac7581c41d
Yaro Shm <yshmarov@gmail.com>
2025-12-06 16:18:13 +0100

    lint

--- config/initializers/simple_form_daisy.rb
+++ config/initializers/simple_form_daisy.rb
@@ -5,7 +5,7 @@
 # components.
 # See https://github.com/heartcombo/simple_form#custom-components to know
 # more about custom components.
-Dir[Rails.root.join("lib/components/**/*.rb")].each { |f| require f }
+Rails.root.glob("lib/components/**/*.rb").each { |f| require f }
 #
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
