d0f3b4864b7acfff83d9c1d52f6797efdab8dc72
Yaro Shm <yshmarov@gmail.com>
2025-12-06 22:27:21 +0100

    cleanup

--- app/controllers/users/invitation_acceptances_controller.rb
+++ app/controllers/users/invitation_acceptances_controller.rb
@@ -7,7 +7,7 @@ class Users::InvitationAcceptancesController < ApplicationController
   before_action :set_user_from_token, only: %i[show update]
 
   def show
-    if @user.nil?
+    if @user.nil? || invitation_already_accepted?
       redirect_to new_user_session_path, alert: t(".invalid_token")
       return
     end
@@ -15,17 +15,16 @@ class Users::InvitationAcceptancesController < ApplicationController
   end
 
   def update
-    if @user.nil?
+    if @user.nil? || invitation_already_accepted?
       redirect_to new_user_session_path, alert: t("users.invitation_acceptances.show.invalid_token")
       return
     end
 
-    if @user.update(password_params)
-      @user.update!(
-        invitation_accepted_at: Time.current,
-        invitation_token: nil,
-        confirmed_at: Time.current
-      )
+    if @user.update(password_params.merge(
+                      invitation_accepted_at: Time.current,
+                      invitation_token: nil,
+                      confirmed_at: Time.current
+                    ))
       sign_in(@user)
       redirect_to user_invitations_path, notice: t(".success")
     else
@@ -38,6 +37,14 @@ class Users::InvitationAcceptancesController < ApplicationController
 
   def set_user_from_token
     @user = User.find_by(invitation_token: params[:invitation_token])
+    # Check if token is expired (7 days)
+    return unless @user&.invitation_created_at && @user.invitation_created_at < 7.days.ago
+
+    @user = nil
+  end
+
+  def invitation_already_accepted?
+    @user&.invitation_accepted_at.present?
   end
 
   def password_params
--- test/controllers/users/invitation_acceptances_controller_test.rb
+++ test/controllers/users/invitation_acceptances_controller_test.rb
@@ -85,4 +85,43 @@ class Users::InvitationAcceptancesControllerTest < ActionDispatch::IntegrationTe
     assert_redirected_to new_user_session_url
     assert_equal I18n.t("users.invitation_acceptances.show.invalid_token"), flash[:alert]
   end
+
+  test "should redirect if invitation already accepted" do
+    inviter = users(:one)
+    organization = organizations(:one)
+    invited_user = User.invite!({ email: "invited@example.com" }, inviter)
+    organization.user_invitations.create!(user: invited_user)
+    invited_user.update!(invitation_accepted_at: Time.current)
+
+    get accept_user_invitation_url(invitation_token: invited_user.invitation_token)
+    assert_redirected_to new_user_session_url
+    assert_equal I18n.t("users.invitation_acceptances.show.invalid_token"), flash[:alert]
+  end
+
+  test "should redirect if token expired" do
+    inviter = users(:one)
+    organization = organizations(:one)
+    invited_user = User.invite!({ email: "invited@example.com" }, inviter)
+    organization.user_invitations.create!(user: invited_user)
+    invited_user.update!(invitation_created_at: 8.days.ago)
+
+    get accept_user_invitation_url(invitation_token: invited_user.invitation_token)
+    assert_redirected_to new_user_session_url
+    assert_equal I18n.t("users.invitation_acceptances.show.invalid_token"), flash[:alert]
+  end
+
+  test "should not accept already accepted invitation" do
+    inviter = users(:one)
+    organization = organizations(:one)
+    invited_user = User.invite!({ email: "invited@example.com" }, inviter)
+    organization.user_invitations.create!(user: invited_user)
+    invited_user.update!(invitation_accepted_at: Time.current)
+    invitation_token = invited_user.invitation_token
+
+    patch accept_user_invitation_url(invitation_token:),
+          params: { user: { password: "newpassword123", password_confirmation: "newpassword123" } }
+
+    assert_redirected_to new_user_session_url
+    assert_equal I18n.t("users.invitation_acceptances.show.invalid_token"), flash[:alert]
+  end
 end
