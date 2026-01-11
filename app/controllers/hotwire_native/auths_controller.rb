b256d625cc4a285275efe2f4fe0d72a3cf4911b3
Yaro Shm <yshmarov@gmail.com>
2026-01-11 20:53:36 +0100

    del ocmments

--- app/controllers/hotwire_native/auths_controller.rb
+++ app/controllers/hotwire_native/auths_controller.rb
@@ -4,11 +4,9 @@ class HotwireNative::AuthsController < ApplicationController
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!, only: [:destroy]
 
-  # Hotwire Native sign out
   def destroy
     return head :unauthorized unless user_signed_in?
 
-    # Delete push device if provided (for push notifications)
     current_user.push_devices.find_by(token: params[:notification_token])&.destroy if params[:notification_token].present?
 
     sign_out(current_user)
